8a3271b998702b7f4fbbdd162adfbd4646b53a8a
ak1t0 <ak1t0.git@hotmail.co.jp>
2014-10-18 14:59:01 +0900

    rebase on closure to lambda and let

--- src/clystal/core.clj
+++ src/clystal/core.clj
@@ -3,6 +3,7 @@
 
 (declare immediate-val? lookup-var eval-list -eval)
 (declare lambda-to-exp unique-form let-to-lambda)
+(declare lambda-to-closure closure-to-exp -apply -eval2)
 
 
 (defn immediate-val? [exp]
@@ -10,22 +11,21 @@
 
 (defn unique-form? [exp]
   (let [head (first exp)]
-    (if (or
-         (and (vector? head) (= (first head) :lambda))
-         (= (first exp) :let))
-      true
-      false)))
+    (or
+     (= head :lambda)
+     (= head :closure)
+     (= head :let))))
 
 (defn unique-form [exp env]
   (cond
    (= (first exp) :let) (let-to-lambda exp env)
-   (= (first (first exp)) :lambda) (lambda-to-exp exp env)))
+   (= (first exp) :lambda) (lambda-to-closure exp env)
+   (= (first exp) :closure) (closure-to-exp exp env)
+   ))
 
 
 (defn key-to-function [exp]
   (symbol (name exp)))
-
-
 
 (defn eval-rest [exp env]
   (map -eval exp (repeat (count exp) env)))
@@ -37,60 +37,21 @@
       (lookup-var exp env))
     (if (unique-form? exp)
       (unique-form exp env)
-      (let
-        [fun (-eval (first exp) env)
-         args (eval-rest (rest exp) env)]
-        (apply fun args)))))
+        (let
+          [fun (-eval (first exp) env)
+           args (eval-rest (rest exp) env)]
+          (apply fun args)))))
 
 
-;;(-eval [[:lambda [:x :y] [:+ :x :y]] n m])
-;;
-;;(-eval [:+ :x :y] {:x n :y m})
-
-(defn lambda-to-exp [lambda env]
-  (let [exp (get (first lambda) 2)
-        local-env (zipmap (second (first lambda)) (rest lambda))]
-    ;exp
-    ;(apply (partial vector local-env) env)
-    (-eval exp (apply (partial vector local-env) env))
-    ))
-
-
-;;[:let [[:x m] [:y n]] [:+ :x :y]]
-;;
-;;[[:lambda [:x :y] [:+ :x :y] m n]
-(defn let-to-lambda [let-ex env]
-  (let [params (get let-ex 1)
-        body (get let-ex 2)
-        local-env (apply hash-map (interleave (map first params) (map second params)))]
-   (-eval
-    (into
-     (vector
-      (vector :lambda (vec (map first params)) body))
-     (map second params))
-     local-env
-     )
-  ))
-
-
-;;[:lambda [:x :y] [:+ :x :y]] env-vec
-;;
-;;[:closure [:x :y] [:+ :x :y] env-map]
-
-(defn lambda-to-closure [exp env]
-  (vector :closure (exp 1) (exp 2) (first env)))
-
-;;[:closure [:x :y] [:+ :x :y] {:x m}] [{:x a :y b}]
+;;[:closure [:x :y] [:+ :x :y] {:x m}] [{:x a :y b}]]
 ;;
 ;;(-eval [:+ :x :y] [{:x m :y b}])
 
-(defn closure-to-exp [closure args]
+(defn closure-to-exp [closure env]
   (let [params (closure 1)
         body (closure 2)
         cls-env-map (closure 3)
-        arg-env-map (apply hash-map (interleave params args))
-        new-env-map (merge arg-env-map cls-env-map)]
-    ;(list body new-env)
+        new-env-map (merge (first env) cls-env-map)]
     (-eval body (vector new-env-map))))
 
 
@@ -99,4 +60,6 @@
 (defn lookup-var [exp env]
   (if (function? (key-to-function exp))
     (eval (key-to-function exp))
-    ((first env) exp)))
+    (if (= ((first env) exp) nil)
+      exp
+      ((first env) exp))))
